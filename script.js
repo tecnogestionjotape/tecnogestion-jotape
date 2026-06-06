374317a2d3554d9ec276522e66216afa6a2eea53
tecnogestion-jotape <tecnogestionjotape@gmail.com>
2026-06-05 21:08:04 -0300

    Arreglo scroll

--- script.js
+++ script.js
@@ -8,13 +8,13 @@ window.addEventListener('scroll', function() {
     const header = document.querySelector('header');
     
     // 1. Lógica del Header (Tus animaciones existentes)
-    if (window.scrollY > 50) {
-        header.style.background = 'rgba(3, 8, 18, 0.9)';
-        header.style.padding = '10px 80px';
-    } else {
-        header.style.background = 'transparent';
-        header.style.padding = '20px 80px';
-    }
+   if (window.scrollY > 50) {
+    header.style.background = 'rgba(3, 8, 18, 0.9)';
+    header.style.padding = window.innerWidth <= 600 ? '12px 16px' : '10px 80px';
+} else {
+    header.style.background = 'transparent';
+    header.style.padding = window.innerWidth <= 600 ? '12px 16px' : '20px 80px';
+}
 
     // 2. Lógica del Botón Volver Arriba
     if (window.scrollY > 300) {
